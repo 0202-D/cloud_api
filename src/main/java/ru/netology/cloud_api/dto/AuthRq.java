110487a8be690aa47e7927b0f5e9e0bfe07d2ae8
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-02-24 10:18:52 +0300

    add container tests

--- src/main/java/ru/netology/cloud_api/dto/AuthRq.java
+++ src/main/java/ru/netology/cloud_api/dto/AuthRq.java
@@ -1,12 +1,14 @@
 package ru.netology.cloud_api.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthRq {
     private String login;
     private String password;
