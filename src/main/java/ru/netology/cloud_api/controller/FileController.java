c3de59593a422c23ffa087f00b4dfbc6b81113c9
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-02-26 16:07:06 +0300

    refactoring

--- src/main/java/ru/netology/cloud_api/controller/FileController.java
+++ src/main/java/ru/netology/cloud_api/controller/FileController.java
@@ -11,7 +11,6 @@ import ru.netology.cloud_api.service.fileservice.FileService;
 import ru.netology.cloud_api.service.authservice.AuthService;
 import ru.netology.cloud_api.service.userservice.UserService;
 
-import javax.validation.constraints.NotBlank;
 import java.io.IOException;
 import java.util.Map;
 
@@ -35,15 +34,14 @@ public class FileController {
 
 
     @PostMapping("/file")
-    public ResponseEntity<?> uploadFile(@RequestHeader("auth-token") String authToken,
-                                        @RequestParam("filename") String filename,
+    public ResponseEntity<?> uploadFile(@RequestParam("filename") String filename,
                                         @RequestBody MultipartFile file) throws IOException {
         fileService.upload(filename, file);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/file")
-    public ResponseEntity<byte[]> getFile(@RequestHeader("auth-token") @NotBlank String authToken, @RequestParam("filename") String filename) {
+    public ResponseEntity<byte[]> getFile(@RequestParam("filename") String filename) {
         File file = fileService.getFile(filename);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
@@ -51,20 +49,20 @@ public class FileController {
     }
 
     @GetMapping("/list")
-    public Object showSavedFiles(@RequestHeader("auth-token") String authToken, @RequestParam("limit") int limit) {
+    public Object showSavedFiles(@RequestParam("limit") int limit) {
         String login = SecurityContextHolder.getContext().getAuthentication().getName();
         return fileService.show(login, limit);
     }
 
     @PutMapping("/file")
-    public ResponseEntity<?> updateFileName(@RequestHeader("auth-token") String authToken, @RequestParam("filename") String fileName,
+    public ResponseEntity<?> updateFileName(@RequestParam("filename") String fileName,
                                             @RequestBody Map<String, String> newFileNameRq) {
         fileService.updateFileName(fileName, newFileNameRq.get("filename"));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/file")
-    public ResponseEntity<?> delete(@RequestHeader("auth-token") String authToken, @RequestParam("filename") String fileName
+    public ResponseEntity<?> delete(@RequestParam("filename") String fileName
     ) {
         fileService.delete(fileName);
         return ResponseEntity.status(HttpStatus.OK).build();
